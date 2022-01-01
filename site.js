56b386a0b596c324187aff6847fd2eb617f838c1
Naveen Alavilli <alavilli@outlook.com>
2022-01-01 17:11:24 -0600

    upsdated refereccnes

--- site.js
+++ site.js
@@ -4,7 +4,7 @@ if ('serviceWorker' in navigator) {
     console.log('Service Worker is supported');
     window.addEventListener('load', () => {
         // Register Service Worker once the window is loaded
-        navigator.serviceWorker.register('../sw_cached_pages.js').
+        navigator.serviceWorker.register('sw_cached_pages.js').
             // navigator.serviceWorker.register('../sw_cached_site.js').
             then(reg => { console.log('Serice worker registered') })
             .catch(err => { console.log('Service worker not registered', err) });
